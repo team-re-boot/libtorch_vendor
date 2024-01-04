8bf682b3d7ac3d21d35ff2836170df49ce8978a9
Masaya Kataoka <ms.kataoka@gmail.com>
2024-01-05 02:12:03 +0900

    add bert tokenizer package
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ bert_tokenizer/include/bert_tokenizer/tokenizer.hpp
@@ -0,0 +1,102 @@
+#include <map>
+#include <string>
+#include <vector>
+
+std::vector<std::string> whitespace_tokenize(std::string text);
+
+std::map<std::string, int> read_vocab(const char * filename);
+
+class BasicTokenizer
+{
+public:
+  bool do_lower_case_;
+  std::vector<std::string> never_split_;
+
+  BasicTokenizer(
+    bool do_lower_case = false,
+    std::vector<std::string> never_split = {"[UNK]", "[SEP]", "[PAD]", "[CLS]", "[MASK]"})
+  {
+    do_lower_case_ = do_lower_case;
+    never_split_ = never_split;
+  }
+
+  std::string _clean_text(std::string text);
+
+  std::vector<std::string> _run_split_on_punc(std::string text);
+
+  std::string _run_strip_accents(std::string text);
+
+  std::string _tokenize_chinese_chars(std::string text);
+
+  std::string utf8chr(int cp);
+
+  bool _is_chinese_char(int cp);
+
+  std::vector<std::string> tokenize(std::string text);
+
+  void truncate_sequences(
+    std::vector<std::string> & textA, std::vector<std::string> & textB,
+    const char * truncation_strategy, size_t max_seq_length);
+};
+
+class WordpieceTokenizer
+{
+public:
+  std::map<std::string, int> vocab_;
+  std::string unk_token_;
+  int max_input_chars_per_word_;
+
+  WordpieceTokenizer(){};
+
+  WordpieceTokenizer(
+    std::map<std::string, int> vocab, std::string unk_token = "[UNK]",
+    int max_input_chars_per_word = 100)
+  {
+    vocab_ = vocab;
+    unk_token_ = unk_token;
+    max_input_chars_per_word_ = max_input_chars_per_word;
+  }
+
+  void add_vocab(std::map<std::string, int> vocab);
+
+  std::vector<std::string> tokenize(std::string text);
+};
+
+class BertTokenizer
+{
+public:
+  std::map<std::string, int> vocab;
+  std::map<int, std::string> ids_to_tokens;
+  bool do_lower_case_;
+  bool do_basic_tokenize_;
+  size_t maxlen_;
+  BasicTokenizer basic_tokenizer;
+  WordpieceTokenizer wordpiece_tokenizer;
+
+  BertTokenizer(){};
+
+  BertTokenizer(
+    const char * vocab_file, bool do_lower_case = false, int max_len = 512,
+    bool do_basic_tokenize = true,
+    std::vector<std::string> /*never_split*/ = {"[UNK]", "[SEP]", "[PAD]", "[CLS]", "[MASK]"})
+  {
+    vocab = read_vocab(vocab_file);
+    for (std::map<std::string, int>::iterator i = vocab.begin(); i != vocab.end(); ++i)
+      ids_to_tokens[i->second] = i->first;
+    do_basic_tokenize_ = do_basic_tokenize;
+    do_lower_case_ = do_lower_case;
+    wordpiece_tokenizer.add_vocab(vocab);
+    maxlen_ = max_len;
+  }
+
+  void add_vocab(const char * vocab_file);
+
+  std::vector<std::string> tokenize(std::string text);
+
+  std::vector<float> convert_tokens_to_ids(std::vector<std::string> tokens);
+
+  void encode(
+    std::string textA, std::string textB, std::vector<float> & input_ids,
+    std::vector<float> & input_mask, std::vector<float> & segment_ids, size_t max_seq_length = 512,
+    const char * truncation_strategy = "longest_first");
+};
